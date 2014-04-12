ad322fc9dd21f700b34cae2055e396709381e360
mefcorvi <mefcorvi@gmail.com>
2014-04-12 15:56:49 +0400

    Текст лицензии немного компактифицирован.

--- MirGames.Domain.Users/CommandHandlers/RemoveOnlineUserTagCommandHandler.cs
+++ MirGames.Domain.Users/CommandHandlers/RemoveOnlineUserTagCommandHandler.cs
@@ -1,11 +1,8 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright company="MirGames" file="RemoveOnlineUserTagCommandHandler.cs">
 // Copyright 2014 Bulat Aykaev
-// 
 // This file is part of MirGames.
-// 
 // MirGames is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// 
 // MirGames is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with MirGames. If not, see http://www.gnu.org/licenses/.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
