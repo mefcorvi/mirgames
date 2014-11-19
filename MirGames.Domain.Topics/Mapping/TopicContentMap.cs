defa771bf03d13ca2df919b7f8ace4bda2b37fd9
mefcorvi <mefcorvi@gmail.com>
2014-11-19 23:54:30 +0300

    Изменение имени таблицы для топиков

--- MirGames.Domain.Topics/Mapping/TopicContentMap.cs
+++ MirGames.Domain.Topics/Mapping/TopicContentMap.cs
@@ -34,7 +34,7 @@ namespace MirGames.Domain.Topics.Mapping
             this.Property(t => t.TopicExtra).IsRequired().HasMaxLength(65535);
 
             // Table & Column Mappings
-            this.ToTable("prefix_topic_content");
+            this.ToTable("topic_content");
             this.Property(t => t.TopicId).HasColumnName("topic_id");
             this.Property(t => t.TopicText).HasColumnName("topic_text");
             this.Property(t => t.TopicTextShort).HasColumnName("topic_text_short");
