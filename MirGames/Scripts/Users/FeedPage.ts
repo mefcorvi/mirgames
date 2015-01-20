64e2e35b29b82b3ef2eb4279a3043c96539dfd28
mefcorvi <mefcorvi@gmail.com>
2015-01-21 02:32:34 +0300

    Кнопка "Отметить всё как прочитанное" в ленте не должна выводиться, если непрочитанных нотификаций нет.

--- MirGames/Scripts/Users/FeedPage.ts
+++ MirGames/Scripts/Users/FeedPage.ts
@@ -7,15 +7,31 @@ module MirGames.Users {
 
     export interface IFeedPageScope extends IPageScope {
         markAllAsRead(): void;
+        unreadItemsCount: number;
     }
 
     export class FeedPage extends MirGames.BasePage<IFeedPageData, IFeedPageScope> {
-        static $inject = ['$scope', 'apiService', 'eventBus'];
+        static $inject = ['$scope', 'apiService', 'eventBus', 'unreadItemsService'];
 
-        constructor($scope: IFeedPageScope, private apiService: Core.IApiService, eventBus: Core.IEventBus) {
+        constructor($scope: IFeedPageScope,
+            private apiService: Core.IApiService,
+            eventBus: Core.IEventBus,
+            private unreadItemsService: IUnreadItemsService) {
             super($scope, eventBus);
 
             $scope.markAllAsRead = () => this.markAllAsRead();
+            $scope.unreadItemsCount = unreadItemsService.getUnreadCount();
+
+            var unreadCountChanged = (unreadCount: number) => {
+                this.$scope.unreadItemsCount = unreadCount;
+                UI.safeDigest(this.$scope);
+            }
+
+            this.unreadItemsService.addHandler(unreadCountChanged);
+
+            this.$scope.$on('$destroy', () => {
+                this.unreadItemsService.removeHandler(unreadCountChanged);
+            });
         }
 
         private markAllAsRead(): void {
